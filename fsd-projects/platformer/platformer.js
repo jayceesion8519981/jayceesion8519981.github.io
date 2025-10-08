e4bfd799dd8e1796849068eb32634cdc1b5c9a62
jayceesion8519981 <jrs8519981@lpssonline.com>
2025-10-08 15:27:16 +0000

    10/8

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,10 +27,15 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     toggleGrid();
 
 
     // TODO 2 - Create Platforms
+    createPlatform(113,478,300,17)
+    createPlatform(300,170,290,13)
+    createPlatform(390,589,655,12)
+    createPlatform(245,345,650,19)
+    createPlatform(358,670,245,15)
 
 
 
